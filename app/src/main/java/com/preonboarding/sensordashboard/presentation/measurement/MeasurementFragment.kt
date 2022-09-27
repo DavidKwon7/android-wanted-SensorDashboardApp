1a28b8fec21b2306742df15539edc0e46c713b31
sujin-kk <sjoo0002@gmail.com>
2022-09-28 00:43:16 +0900

    [FEAT] collect measure target

--- app/src/main/java/com/preonboarding/sensordashboard/presentation/measurement/MeasurementFragment.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/presentation/measurement/MeasurementFragment.kt
@@ -3,6 +3,7 @@ package com.preonboarding.sensordashboard.presentation.measurement
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.preonboarding.sensordashboard.R
 import com.preonboarding.sensordashboard.databinding.FragmentMeasurementBinding
 import com.preonboarding.sensordashboard.presentation.common.base.BaseFragment
@@ -15,11 +16,18 @@ class MeasurementFragment : BaseFragment<FragmentMeasurementBinding>(R.layout.fr
         super.onViewCreated(view, savedInstanceState)
 
         initViews()
+        bindingViewModel()
     }
 
     private fun bindingViewModel() {
         binding.viewModel = viewModel
         binding.measureTarget = viewModel.curMeasureTarget.value
+
+        lifecycleScope.launchWhenCreated {
+            viewModel.curMeasureTarget.collect {
+                binding.measureTarget = it
+            }
+        }
     }
 
     private fun initViews() {

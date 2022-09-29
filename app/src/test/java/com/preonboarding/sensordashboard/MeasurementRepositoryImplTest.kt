555564832e1d74d33139cd358c72303122987d83
DavidKwon7 <khjunejune10@gmail.com>
2022-09-30 03:22:20 +0900

    [FEAT] Repository: get_data_success 테스트

--- app/src/test/java/com/preonboarding/sensordashboard/MeasurementRepositoryImplTest.kt
+++ app/src/test/java/com/preonboarding/sensordashboard/MeasurementRepositoryImplTest.kt
@@ -18,12 +18,14 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import java.io.IOException
+import java.lang.Error
 import kotlin.time.ExperimentalTime
 
 @ExperimentalCoroutinesApi
@@ -50,52 +52,18 @@ class MeasurementRepositoryImplTest {
         )
     }
 
-
-    @OptIn(ExperimentalTime::class)
     @Test
     fun test_get_data_success() = runTest {
         val data = TestDataGenerator.generateMeasurementEntityList()
-
         coEvery { measurementDAO.getAllMeasurement() } returns data
-
-        val flow = measurementRepository.getAllMeasurement()
-
-        flow.test {
-            val expected = expectItem()
-
-            Truth.assertThat(expected).isEqualTo(data)
-        }
-    }
-
-
-    //test1_fail_case
-    @Test
-    fun test_fail() = runTest {
-        coEvery { measurementDAO.getAllMeasurement() } throws Exception()
-        val api = measurementRepository.getAllMeasurement()
-        Truth.assertThat(api).isEqualTo(TestDataGenerator.generateMeasurementEntityList())
+        val expectData = measurementDAO.getAllMeasurement()
         coVerify { measurementDAO.getAllMeasurement() }
+
+        Assert.assertEquals(
+            data, expectData
+        )
     }
 
-/*    // todo edit this code => flow 로 인해 데이터 확인을 못 하고 있음 ..
-    @Test
-    fun test_get_all_measurement_success() = runTest {
-        val getItems = TestDataGenerator.generateMeasurementEntityList()
-        coEvery { measurementDAO.getAllMeasurement() } returns(getItems)
-        val returned = measurementRepository.getAllMeasurement()
-        //coVerify { measurementDAO.getAllMeasurement() }
-        //Truth.assertThat(returned).containsExactlyElementsIn(getItems)
-        Truth.assertThat(returned).isEqualTo(flowData())
-    }
-
-    @Test(expected = Exception::class)
-    fun test_get_all_measurement_fail() = runTest {
-        coEvery { measurementDAO.getAllMeasurement() } throws Exception()
-        measurementRepository.getAllMeasurement()
-        coVerify { measurementDAO.getAllMeasurement() }
-    }
-
-    // return 값이 Uni 이여서 일단 Unit 처리
     @Test
     fun test_save_measurement_success() = runTest {
 
@@ -107,7 +75,6 @@ class MeasurementRepositoryImplTest {
 
         coVerify { measurementDAO.saveMeasurement(any()) }
 
-        // assertion
         Truth.assertThat(returned).isEqualTo(Unit)
     }
 
@@ -116,15 +83,12 @@ class MeasurementRepositoryImplTest {
 
         val saveItem = TestDataGenerator.generateSensorInfoList()
 
-        // Given
         coEvery { measurementDAO.saveMeasurement(any()) } throws Exception()
 
-        // When
         measurementRepository.saveMeasurement(saveItem,"a","a",1.1)
 
-        // Then
         coVerify { measurementDAO.saveMeasurement(any()) }
 
-    }*/
+    }
 
 }

ab36b6e0f4ad7126ee4060b093b0470ab73a7dc2
seoyoon513 <110798031+seoyoon513@users.noreply.github.com>
2022-09-30 11:05:36 +0900

    [FEAT] type별로 ui 상태 변경

--- app/src/main/java/com/preonboarding/sensordashboard/presentation/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/presentation/dashboard/DashboardFragment.kt
@@ -2,6 +2,7 @@ package com.preonboarding.sensordashboard.presentation.dashboard
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -9,8 +10,10 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.preonboarding.sensordashboard.R
 import com.preonboarding.sensordashboard.databinding.FragmentDashboardBinding
+import com.preonboarding.sensordashboard.domain.model.ViewType
 import com.preonboarding.sensordashboard.presentation.common.base.BaseFragment
 import com.preonboarding.sensordashboard.presentation.common.util.NavigationUtil.navigate
+import com.preonboarding.sensordashboard.presentation.replay.ReplayViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -20,6 +23,7 @@ import timber.log.Timber
 class DashboardFragment : BaseFragment<FragmentDashboardBinding>(R.layout.fragment_dashboard) {
 
     private val viewModel: DashboardViewModel by viewModels()
+    private val replayViewModel: ReplayViewModel by activityViewModels()
     private val pagingAdapter: DashboardPagingAdapter by lazy { DashboardPagingAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,6 +54,7 @@ class DashboardFragment : BaseFragment<FragmentDashboardBinding>(R.layout.fragme
 
         btnReplay.setOnClickListener {
             navigate(R.id.action_dashboard_to_replay)
+            replayViewModel.setReplayViewType(ViewType.REPLAY)
         }
     }
 
--- app/src/main/java/com/preonboarding/sensordashboard/presentation/replay/ReplayFragment.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/presentation/replay/ReplayFragment.kt
@@ -2,17 +2,55 @@ package com.preonboarding.sensordashboard.presentation.replay
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.preonboarding.sensordashboard.R
 import com.preonboarding.sensordashboard.databinding.FragmentReplayBinding
 import com.preonboarding.sensordashboard.presentation.common.base.BaseFragment
 import com.preonboarding.sensordashboard.presentation.common.util.NavigationUtil.navigateUp
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class ReplayFragment : BaseFragment<FragmentReplayBinding>(R.layout.fragment_replay) {
+
+    private val viewModel: ReplayViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        registerObserver()
         initViews()
+    }
+
+    private fun registerObserver() {
+        binding.viewmodel = viewModel
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.curViewType.collect {
+                    binding.viewType = it
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.curPlayType.collect {
+                    binding.playType = it
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.timerCount.collect {
+                    binding.timerCount = it
+                }
+            }
+        }
     }
 
     private fun initViews() {
